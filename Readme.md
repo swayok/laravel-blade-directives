0b5d74fe272fd7ce1e98e3c83662fa723cde6805
Alexander Filippov <swayalex@gmail.com>
2019-03-30 18:30:46 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,257 @@
+# What is this
+This package provides additional directived for Laravel Blade template engingine. 
+Among directives are fully-functional partials, translations and [dot.js](http://olado.github.io/doT/index.html) 
+constructions.
+
+## Installation 
+
+Add require to `composer.json` and run `composer update`
+
+    "require": {
+        "swayok/laravel-blade-directives": "~1.0",
+    }
+
+## Configuration
+
+### Service provider for Laravel 5.6+
+
+Automatically added via package auto-discovery.
+
+### Service provider for Laravel < 5.6
+
+Add `\LaravelBladeDirectives\LaravelBladeDirectivesServiceProvider::class` to your `config/app.php` into `providers` array
+
+## Shortcuts
+
+    @breakpoint 
+    // add breakpoint to view (allows you to debug compiled views)
+    
+    @string($id, $parameters = [], $locale = null)
+    @trans($id, $parameters = [], $locale = null)
+    // {{ trans($id, $parameters, $locale) }}
+    
+    @html($id, $parameters = [], $locale = null)
+    @unescaped($id, $parameters = [], $locale = null)
+    // {!! trans($id, $parameters, $locale) !!}
+    
+    @key($array, $key, $default, $escaped = true)
+    // {{ array_get($array, $key, $default) }} - when $escaped === true
+    // {!! array_get($array, $key, $default) !!} - when $escaped === false
+    
+    Examples:
+    
+    @string('frontend.footer', ['year' => 2019], 'en')
+    @trans('frontend.footer', ['year' => 2019])
+    @html('frontend.lang.' . app()->locale())
+    @unescaped('frontend.app_name')
+    
+    @key($settings, 'language', 'en') 
+    // {{ array_get($settings, 'language', 'en') }}
+    @key($settings, 'language', 'en', false)
+    // {!! array_get($settings, 'language', 'en') !!}
+
+## Partials
+
+The idea behind partials is possibility to use sub-template several times in a single blade template 
+wihtout using a `@view()` directives that require a view file to be created. Sometimes you do not 
+need separate view file for simple template like table row. 
+Partials also useful to make big template a bit cleaner. 
+
+Partials declared and used like sections with exception that partials receive only data
+you explicitely pass into `@yieldPartial` and some shared data provided by `$__env->getShared()` (`\Illuminate\View\Factory->getShared()`).
+Under the hood partial is a closure that renders some blade template using passed data. 
+It does not create files and all code is placed inside current blade template.
+
+### Declare partial
+    
+    @partial('name')
+        <div>{{ $var1 }}</div>
+    @endPartial
+    
+### Yield partial
+
+    @yieldPartial('name', ['var1' => 'this is partial'])
+    @yieldpartial('name', ['var1' => 'this is another partial'])
+    
+### Result
+    
+    <div>this is partial</div>
+    <div>this is another partial</div>
+    
+Note: all directives have both cameCase and lowercase versions
+
+## [dot.js](http://olado.github.io/doT/index.html) directives
+It is a pain using dot.js inserts inside blade templates. 
+You need to add `@` before each dot.js insert, also you cannot insert php code inside such inserts.
+Template becomes a real mess sometimes. 
+
+The idea with this directives is to make blade templates that contain dot.js inserts 
+more clean and visually better. In addition there must be a possibility to insert
+some php code inside directive.
+
+### Simple inserts
+
+    @jsEcho(it.value)
+    // {{= it.value }}
+    @jsecho(it.value || 'default')
+    // {{= it.value || 'default' }}
+    
+    @jsEchoEncoded(it.value)
+    // {{! it.value }}
+    @jsechoencoded(it.value || "default")
+    // {{! it.value || "default" }}
+    
+    @jsEval(it._some_name = 'value')
+    // {{ it._some_name = 'value' }}
+    @jseval(it._some_name = "value")
+    // {{ it._some_name = "value" }}
+    
+    @jsIf(it.value)
+    // {{? it.value }}
+    @jsif(it.value === 'example')
+    // {{? it.value === 'example' }}
+    
+    @jsIfSimple(it.value)
+    // {{? it.value }}
+    @jsifsimple(it.value === 'example')
+    // {{? it.value === 'example' }}
+    
+    @jsElseIf(it.value)
+    // {{?? it.value }}
+    @jselseif(it.value === 'example')
+    // {{?? it.value === 'example' }}
+    
+    @jsElseIfSimple(it.value)
+    // {{?? it.value }}
+    @jselseifsimple(it.value === 'example')
+    // {{?? it.value === 'example' }}
+    
+    @jsElse
+    @jselse
+    // {{??}}
+    
+    @jsEndIf
+    @jsendif
+    // {{?}}
+    
+    @jsForEach(it.array as key => value)
+    // {{~ it.array :value:key }}
+    @jsforeach(it.array as value)
+    // {{~ it.array :value }}
+    
+    @jsEndForEach
+    @jsendforeach
+    // {{~}}
+    
+### Complex inserts with PHP code
+PHP code inside dot.js directive must begin with `` `. `` and end with `` .` `` 
+(the quote is the one that placed on tilda `~` button on keyboard and is called `backtick`).
+
+Using backticks allows using normal quotes and double quotes without problems caused by incorrect quotes escaping.
+Also if you're using PHP Storm you can configure Code Style for backticks that differs from other quotes and 
+shows dot.js directives in unique way (configs are in the next section). 
+ 
+Examples:
+
+    $phpVar = 'var_string'
+    SOME_CONSTANT = 'constant_string'
+    $phpNumber = 5
+
+    @jsIf(it.value === "` . $phpVar . `")
+    // {{? it.value === "var_string" }}
+    // Resulting php code in compiled template is: <?php echo '@{{? it.value === "' . ($phpVar) . '" }}'; ?>
+
+    @jsIf(it.value === '` . $phpVar . `')
+    // {{? it.value === 'var_string' }}
+    // Resulting php code in compiled template is: <?php echo '@{{? it.value === \'' . ($phpVar) . '\' }}'; ?>
+    
+    @jsif(it.value === "` . SOME_CONSTANT . `")
+    // {{? it.value === "constant_string" }}
+
+    @jsIf(it.value >= ` . ($phpNumber - 1) . ` && it.value <= ` . ($phpNumber + 1) . `)
+    // {{? it.value >= 4 && it.value <= 6}}
+    
+#### Directives that support PHP code:
+
+- `jsEcho`/`jsecho`
+- `jsechoencoded`/`jsechoencoded`
+- `jsEval`/`jseval`
+- `jsIf`/`jsif`
+- `jsElseIf`/`jselseif`
+
+#### Directives that does not support PHP code:
+
+- `jsIfSimple`/`jsifsimple`
+- `jsElseIfSimple`/`jselseifsimple`
+- `jsForEach`/`jsforeach`
+- Other directives that does not allow parameters (like `jsEndIf`)
+
+## PHP Storm Configuration (Blade directives)
+Open `Settings > Languages & Frameworks > PHP > Blade`. Untick `Use default settings`.
+Open `Directives` tab.
+
+Each directive have next inputs:
+- Name 
+- Has parameter
+- Prefix
+- Suffix
+
+Note: directives with same configs are grouped in Names - you will need to create a
+record for each directive name.
+
+    Names: string, trans, html, unescaped
+    Has parameter: true
+    Prefix: <?php echo trans(
+    Suffix: ); ?>
+    
+    Names: key
+    Has parameter: true
+    Prefix: <?php $fn = function(array $array, $key, $default = null, bool $escaped = true) {} ; $fn(
+    Suffix: ); ?>
+    
+    Names: partial
+    Has parameter: true
+    Prefix: <?php $str =
+    Suffix: ); ?>
+    
+    Names: yieldPartial, yieldpartial
+    Has parameter: true
+    Prefix: <?php $fn = function(string $partialName, array $partialData = []) {}; $fn(
+    Suffix: ); ?>
+    
+    Names: breakpoint, endPartial, endpartial
+    Has parameter: false
+    
+    Names: jsEcho, jsEchoEncoded, jsEval, jsForEach, jsIf, jsElseIf, jsIfSimple, jsElseIfSimple
+    Has parameter: true
+    Prefix: <?php echo `
+    Suffix: `; ?>
+    
+    Names: jsecho, jsechoencoded, jseval, jsforeach, jsif, jselseif, jsifsimple, jselseifsimple
+    Has parameter: true
+    Prefix: <?php echo `
+    Suffix: `; ?>
+    
+    Names: jsElse, jsEndIf, jsEndForEach, jselse, jsendif, jsendforeach
+    Has parameter: false
+
+## PHP Storm Configuration (colors for directives)
+
+Open `Settings > Editor > Color Scheme > Blade`.
+Interesting parameters here are:
+- `Directive`
+- `Text block delimiter`
+
+I added background to both so that directives are clearly visible in Blade template.
+
+Open `Settings > Editor > Color Scheme > PHP`.
+
+Interesting parameter here is `Shell command`. This is rarely used feature in PHP but it
+will be useful for showing dot.js directives differently from usual Blade directives. 
+Just add some background color. This will display parameters within dot.js directive with 
+specific background color. I personally like this possibility because it makes 
+normal and dot.js directives visually different.
+
+
+
+
---
+++ src/LaravelBladeDirectives/LaravelBladeDirectivesServiceProvider.php
@@ -0,0 +1,247 @@
+<?php
+
+namespace LaravelBladeDirectives;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBladeDirectivesServiceProvider extends ServiceProvider {
+
+    public function register() {
+        $this->registerDotJsBladeDirectives();
+        $this->registerUsefulBladeDirectives();
+    }
+
+    /**
+     * Configs for PHP Storm IDE:
+     *
+     * Settings -> Languages & Frameworks -> PHP -> Blade -> Directives
+     *
+     * For directives: partial
+     * Has parameter: true
+     * Prefix: <?php $str =
+     * Suffix: ; ?>
+     *
+     * For directives: yieldPartial, yieldpartial
+     * Has parameter: true
+     * Prefix: <?php $fn = function(string $partialName, array $partialData = []) {}; $fn(
+     * Suffix: ); ?>
+     *
+     * For directives: string, trans, html, unescaped
+     * Has parameter: true
+     * Prefix: <?php echo trans(
+     * Suffix: ); ?>
+     *
+     * For directives: key
+     * Has parameter: true
+     * Prefix: <?php $fn = function(array $array, $key, $default = null, bool $escaped = true) {} ; $fn(
+     * Suffix: ); ?>
+     *
+     * For directives without parameters: breakpoint, endPartial, endpartial
+     * Has parameter: false
+     */
+    protected function registerUsefulBladeDirectives() {
+        // @breakpoint
+        \Blade::directive('breakpoint', function () {
+            return '<?php if(function_exists("xdebug_break")){ xdebug_break(); } ?>';
+        });
+        // @partial('section_name')
+        \Blade::directive('partial', function ($expression) {
+            return <<<PARTIAL
+                <?php \$__partials[$expression] = function (array \$envData, array \$customData) { 
+                    extract(\$envData);
+                    extract(\$customData);
+                ?>
+PARTIAL;
+        });
+        $endPartial = function () {
+            return '<?php } ?>';
+        };
+        // @endPartial
+        \Blade::directive('endPartial', $endPartial);
+        // @endpartial
+        \Blade::directive('endpartial', $endPartial);
+
+        $yieldPartial = function ($expression) {
+            return <<<PARTIAL
+                <?php
+                    \$arguments = [$expression];
+                    array_get(\$__partials, \$arguments[0], function () {})(\$__env->getShared(), (array)array_get(\$arguments, 1, []));
+                ?> 
+PARTIAL;
+        };
+        // @yieldPartial('section_name', ['var1' => 'value1', ...])
+        \Blade::directive('yieldPartial', $yieldPartial);
+        // @yieldpartial('section_name', ['var1' => 'value1', ...])
+        \Blade::directive('yieldpartial', $yieldPartial);
+
+        // @string('path.to.translation')
+        // @trans('path.to.translation')
+        $closure = function ($expression) {
+            return "<?php echo e(trans($expression)); ?>";
+        };
+        \Blade::directive('string', $closure);
+        \Blade::directive('trans', $closure);
+
+
+        // @html('path.to.translation')
+        // @unescaped('path.to.translation')
+        $closure = function ($expression) {
+            return "<?php echo trans($expression); ?>";
+        };
+        \Blade::directive('html', $closure);
+        \Blade::directive('unescaped', $closure);
+
+        // @key($array, 'array key', $default = null, bool $escaped = true)
+        \Blade::directive('key', function ($expression) {
+            $parts = explode(',', $expression);
+            if (isset($parts[3]) && trim($parts[3]) === 'false') {
+                // unescaped
+                return "<?php echo array_get($expression); ?>";
+            } else {
+                return "<?php echo e(array_get($expression)); ?>";
+            }
+        });
+
+    }
+
+    /**
+     * Configs for PHP Storm IDE:
+     *
+     * Settings -> Languages & Frameworks -> PHP -> Blade -> Directives
+     *
+     * For directives: jsEcho, jsEchoEncoded, jsEval, jsForEach, jsIf, jsElseIf, jsIfSimple, jsElseIfSimple
+     * Has parameter: true
+     * Prefix: <?php echo `
+     * Suffix: `; ?>
+     *
+     * For directives without parameters: jsElse, jsEndIf, jsEndForEach
+     * Has parameter: false
+     *
+     * Additional:
+     * Configure how `text in tilda quotes` displayed via
+     * Settings -> Editor -> Color Scheme -> PHP -> Shell command
+     */
+    private function registerDotJsBladeDirectives() {
+        // {{= it.value }}
+        $closure = function ($expression) {
+            $expression = $this->processDotJsExpressionWithPhpCodeInside($expression);
+            return "<?php echo '@{{= {$expression} }}' ?>";
+        };
+        \Blade::directive('jsEcho', $closure);
+        \Blade::directive('jsecho', $closure);
+
+        // {{! it.value }}
+        $closure = function ($expression) {
+            $expression = $this->processDotJsExpressionWithPhpCodeInside($expression);
+            return "<?php echo '@{{! {$expression} }}' ?>";
+        };
+        \Blade::directive('jsEchoEncoded', $closure);
+        \Blade::directive('jsechoencoded', $closure);
+
+        // {{ it._some_name = value }}
+        // {{ it._some_name = '` . $phpVar . `' }}
+        // {{ it._some_name = "` . SOME_CONSTANT . `" }}
+        // {{ it._some_name = "` . ($phpVar + 1) . `" }}
+        // Note that next $expression might work incorrectly!:
+        // it._some_name = "` . $type['value'] . `asdasd` . `"
+        $closure = function ($expression) {
+            $expression = $this->processDotJsExpressionWithPhpCodeInside($expression);
+            return "<?php echo '@{{ {$expression}; }}' ?>";
+        };
+        \Blade::directive('jsEval', $closure);
+        \Blade::directive('jseval', $closure);
+
+        // {{? it.value }}
+        // {{? it.value === '` . $phpVar . `' }}
+        // {{? it.value === "` . SOME_CONSTANT . `" }}
+        // {{? it.value === "` . ($phpVar + 1) . `" }}
+        // Note that next $expression might work incorrectly!:
+        // it.value === "` . $type['value'] . `asdasd` . `"
+        $closure = function ($expression) {
+            $expression = $this->processDotJsExpressionWithPhpCodeInside($expression);
+            return "<?php echo '@{{? {$expression} }}' ?>";
+        };
+        \Blade::directive('jsIf', $closure);
+        \Blade::directive('jsif', $closure);
+
+        // {{? it.value }}
+        // PHP expressions ignored
+        $closure = function ($expression) {
+            $expression = str_replace("'", "\\'", $expression);
+            return "<?php echo '@{{? {$expression} }}' ?>";
+        };
+        \Blade::directive('jsIfSimple', $closure);
+        \Blade::directive('jsifsimple', $closure);
+
+        // {{?? it.value }}
+        // {{?? it.value === '` . $phpVar . `' }}
+        // {{?? it.value === "` . SOME_CONSTANT . `" }}
+        // {{?? it.value === "` . ($phpVar + 1) . `" }}
+        // Note that next $expression might work incorrectly!:
+        // it.value === "` . $type['value'] . `asdasd` . `"
+        $closure = function ($expression) {
+            $expression = $this->processDotJsExpressionWithPhpCodeInside($expression);
+            return "<?php echo '@{{?? {$expression} }}' ?>";
+        };
+        \Blade::directive('jsElseIf', $closure);
+        \Blade::directive('jselseif', $closure);
+
+        // {{?? it.value }}
+        // PHP expressions ignored
+        $closure = function ($expression) {
+            $expression = str_replace("'", "\\'", $expression);
+            return "<?php echo '@{{?? {$expression} }}' ?>";
+        };
+        \Blade::directive('jsElseIfSimple', $closure);
+        \Blade::directive('jselseifsimple', $closure);
+
+        // {{??}}
+        $closure = function () {
+            return "<?php echo '@{{??}}' ?>";
+        };
+        \Blade::directive('jsElse', $closure);
+        \Blade::directive('jselse', $closure);
+
+        // {{?}}
+        $closure = function () {
+            return "<?php echo '@{{?}}' ?>";
+        };
+        \Blade::directive('jsEndIf', $closure);
+        \Blade::directive('jsendif', $closure);
+
+        // {{~ it.array :value:key }} => @jsForEach(it.array as key => value) or @jsForEach(it.array as value)
+        $closure = function ($expression) {
+            if (preg_match('%^(.+) as (.+?)(?:\s*=>\s*(.+))?$%', $expression, $matches)) {
+                // php-styled format of foreach
+                if (empty($matches[3])) {
+                    // $expression = 'it.array as value' => {{~ it.array :value }}
+                    $expression = "{$matches[1]} :{$matches[2]}";
+                } else {
+                    // $expression = 'it.array as key => value' => {{~ it.array :value:key }}
+                    $expression = "{$matches[1]} :{$matches[3]}:{$matches[2]}";
+                }
+            }
+            $expression = str_replace("'", "\\'", $expression);
+            return "<?php echo '@{{~ {$expression} }}' ?>";
+        };
+        \Blade::directive('jsForEach', $closure);
+        \Blade::directive('jsforeach', $closure);
+
+        // {{~}}
+        $closure = function () {
+            return "<?php echo '@{{~}}' ?>";
+        };
+        \Blade::directive('jsEndForEach', $closure);
+        \Blade::directive('jsendforeach', $closure);
+    }
+
+    protected function processDotJsExpressionWithPhpCodeInside($dotJsExpression): string {
+        $expression = str_replace("'", "\\'", $dotJsExpression);
+        // replace {{ $phpExpression }} by "' . ($phpExpression) . '"
+        return preg_replace_callback("%`\s*\.\s*(.+?)\s*\.\s*`%", function ($matches) {
+            $insert = str_replace("\\'", "'", $matches[1]);
+            return "'. ($insert) . '";
+        }, $expression);
+    }
+
+}
